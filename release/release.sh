0d7c77d659328c96d92c4b9ed61abcb27ba29c1a
Tom Schöner <schoener@subshell.com>
2020-04-06 15:13:03 +0200

    use maven and node commands

--- release/release.sh
+++ release/release.sh
@@ -3,8 +3,6 @@
 # Release script
 # ===============
 # Sets the new version for the package.json file, pom.xml and as a git tag.
-# Dependencies:
-# - xmlstarlet (sudo apt-get install xmlstarlet)
 
 versionIdentifier=$1
 if [[ ! $versionIdentifier =~ ^(v[0-9]+(\.[0-9]+)*|major|minor|patch)$ ]]; then
@@ -22,14 +20,14 @@ rootDir="$(dirname "$0")"/..
 frontendDir="${rootDir}"/frontend
 pomDir="${rootDir}"
 
-currentVersion=$(grep -oP '(?<="version": ")[^"]*' "${frontendDir}"/package.json)
+currentVersion=$(node -pe "require('${frontendDir}/package.json').version")
 
 # update version (package.json)
 nextTag=$(cd "${frontendDir}" && npm version "${versionIdentifier}")
 nextVersion="${nextTag:1}"
 
 # update version (pom.xml)
-xmlstarlet ed --inplace -N a="http://maven.apache.org/POM/4.0.0" -u '/a:project/a:version' -v "${nextVersion}" "${pomDir}"/pom.xml
+mvn versions:set -DnewVersion="${nextVersion}" -DgenerateBackupPoms=false
 
 # git tag
 git add "${frontendDir}/package.json" "${pomDir}/pom.xml"
