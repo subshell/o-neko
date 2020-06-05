e16e051134c26cb882199117196ce7a00be5401d
Philip Dakowitz <dakowitz@subshell.com>
2020-06-05 15:19:30 +0200

    Replace main font, add two tone icons as an option and move mdi icons into the mdi namespace

--- frontend/src/app/configuration/configuration.ts
+++ frontend/src/app/configuration/configuration.ts
@@ -1,11 +1,15 @@
 import {MatIconRegistry} from '@angular/material/icon';
 import {DomSanitizer} from '@angular/platform-browser';
 import {MDI_SVG_ICONS} from './generated/mdi';
+import {TWO_TONE_ICONS} from './generated/two-tone';
 
 export const configureSvgIcons = (iconRegistry: MatIconRegistry, domSanitizer: DomSanitizer) => {
   // If the following produces errors in your IDE you need to install the npm dependencies
   // or run the postinstall script (O-NEKO-ROOT/frontend/extract-svg-icons.js) manually via node
   MDI_SVG_ICONS.icons.forEach(svgIcon => {
-    iconRegistry.addSvgIcon(svgIcon, domSanitizer.bypassSecurityTrustResourceUrl(`${MDI_SVG_ICONS.basePath}/${svgIcon}.svg`));
+    iconRegistry.addSvgIconInNamespace('mdi', svgIcon, domSanitizer.bypassSecurityTrustResourceUrl(`${MDI_SVG_ICONS.basePath}/${svgIcon}.svg`));
+  });
+  TWO_TONE_ICONS.icons.forEach(svgIcon => {
+    iconRegistry.addSvgIcon(svgIcon, domSanitizer.bypassSecurityTrustResourceUrl(`${TWO_TONE_ICONS.basePath}/${svgIcon}.svg`));
   });
 };
