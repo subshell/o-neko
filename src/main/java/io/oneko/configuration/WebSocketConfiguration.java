784ecdba439af85cdc43d4d6f904396c6e188756
Tom Schöner <schoener@subshell.com>
2020-11-18 11:48:11 +0100

    WebSocketConfiguration

--- src/main/java/io/oneko/configuration/WebSocketConfiguration.java
+++ src/main/java/io/oneko/configuration/WebSocketConfiguration.java
@@ -1,36 +1,27 @@
 package io.oneko.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.HandlerMapping;
-import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
-import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
-import java.util.HashMap;
-import java.util.Map;
+import io.oneko.websocket.SessionWebSocketHandler;
 
 
-/**
- * Inspired by http://www.baeldung.com/spring-5-reactive-websockets
- */
 @Configuration
-public class WebSocketConfiguration {
-	private final WebSocketHandler webSocketHandler;
+@EnableWebSocket
+public class WebSocketConfiguration implements WebSocketConfigurer {
+
+	private final SessionWebSocketHandler webSocketHandler;
 
 	@Autowired
-	public WebSocketConfiguration(WebSocketHandler webSocketHandler) {
+	public WebSocketConfiguration(SessionWebSocketHandler webSocketHandler) {
 		this.webSocketHandler = webSocketHandler;
 	}
 
-	@Bean
-	public HandlerMapping webSocketHandlerMapping() {
-		Map<String, WebSocketHandler> map = new HashMap<>();
-		map.put("/ws", webSocketHandler);
-
-		SimpleUrlHandlerMapping handlerMapping = new SimpleUrlHandlerMapping();
-		handlerMapping.setOrder(1);
-		handlerMapping.setUrlMap(map);
-		return handlerMapping;
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(webSocketHandler, "/ws").setAllowedOrigins("*");
 	}
 }
