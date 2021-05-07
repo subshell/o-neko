b0afab2041b2aabbb2f31f92f14c9ba7158817ee
Philip Dakowitz <dakowitz@subshell.com>
2021-05-07 15:00:21 +0200

    update guava + fabric8 k8s client, remove unused docker-client dependency

--- src/main/java/io/oneko/docker/v2/model/manifest/DockerRegistryManifest.java
+++ src/main/java/io/oneko/docker/v2/model/manifest/DockerRegistryManifest.java
@@ -1,6 +1,6 @@
 package io.oneko.docker.v2.model.manifest;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
--- src/main/java/io/oneko/kubernetes/impl/KubernetesAccess.java
+++ src/main/java/io/oneko/kubernetes/impl/KubernetesAccess.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
