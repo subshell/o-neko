12cad6be4790ce7c984ca4b3c2b5797f457eb0bb
Philip Dakowitz <dakowitz@subshell.com>
2021-08-27 15:44:26 +0200

    localized dates

--- frontend/src/app/store/i18n/i18n.state.ts
+++ frontend/src/app/store/i18n/i18n.state.ts
@@ -1,8 +1,15 @@
-import {Action, NgxsOnInit, Selector, State, StateContext, Store} from "@ngxs/store";
+import {Action, Selector, State, StateContext, Store} from "@ngxs/store";
 import {Injectable} from "@angular/core";
 import {TranslateService} from "@ngx-translate/core";
 
 export type OnekoLocale = 'de' | 'en';
+
+// simple hack to allow the date-format-function from date-time-parser.ts to use the locale without
+// the need for an angular service etc.
+export function currentOnekoLocale(): OnekoLocale {
+  return currentLocale;
+}
+let currentLocale: OnekoLocale;
 
 export interface I18nStateModel {
   locale?: OnekoLocale
@@ -29,11 +36,16 @@ export class I18nState {
 
   private static defaultLocale: OnekoLocale;
 
-  constructor(private translate: TranslateService) {
+  constructor(private translate: TranslateService, private store: Store) {
     switch(translate.getBrowserLang()) {
       case 'de': I18nState.defaultLocale = 'de'; break;
       default: I18nState.defaultLocale = 'en';
     }
+    this.initGlobalVariable();
+  }
+
+  private initGlobalVariable() {
+    this.store.select(I18nState.locale).subscribe(locale => currentLocale = locale);
   }
 
   @Selector()
@@ -44,6 +56,7 @@ export class I18nState {
   @Action(SetLocale)
   setLocale(ctx: StateContext<I18nStateModel>, action: SetLocale) {
     ctx.setState({locale: action.locale})
+    currentLocale = action.locale;
   }
 
 }
