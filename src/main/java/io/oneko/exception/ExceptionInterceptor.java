17bd0381d1233588f469f238811339f0f0cae343
Tom Schoener <schoener@subshell.com>
2022-06-10 17:04:04 +0200

    return message for IllegalArgumentExceptions

--- src/main/java/io/oneko/exception/ExceptionInterceptor.java
+++ src/main/java/io/oneko/exception/ExceptionInterceptor.java
@@ -11,8 +11,8 @@ import io.oneko.helm.HelmRegistryException;
 public class ExceptionInterceptor {
 
 	@ExceptionHandler(value = IllegalArgumentException.class)
-	public ResponseEntity badRequest() {
-		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+	public ResponseEntity<?> badRequest(IllegalArgumentException e) {
+		return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
 	}
 
 	@ExceptionHandler(value = HelmRegistryException.class)
