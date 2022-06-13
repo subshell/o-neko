7c6c7df31879b2d7b80eb0d17ba93b9364b2d868
Tom Schoener <schoener@subshell.com>
2022-06-13 08:41:40 +0200

    improve error handling

--- src/main/java/io/oneko/exception/ExceptionInterceptor.java
+++ src/main/java/io/oneko/exception/ExceptionInterceptor.java
@@ -10,13 +10,21 @@ import io.oneko.helm.HelmRegistryException;
 @ControllerAdvice
 public class ExceptionInterceptor {
 
+	@ExceptionHandler(value = RuntimeException.class)
+	public ResponseEntity<?> badRequest(RuntimeException e) {
+		if (e.getCause() instanceof HelmRegistryException) {
+			return badRequestForHelmRegistry((HelmRegistryException) e.getCause());
+		}
+		return new ResponseEntity<>("An unexpected error occurred while handling the request", HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
 	@ExceptionHandler(value = IllegalArgumentException.class)
 	public ResponseEntity<?> badRequest(IllegalArgumentException e) {
 		return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
 	}
 
 	@ExceptionHandler(value = HelmRegistryException.class)
-	public ResponseEntity<String> badRequestForHelmRegistry(HelmRegistryException e) {
+	public ResponseEntity<?> badRequestForHelmRegistry(HelmRegistryException e) {
 		return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
 	}
 
