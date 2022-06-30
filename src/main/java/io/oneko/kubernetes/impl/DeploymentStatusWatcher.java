7e6cfbe7c5d7e05d5b1cf34609b227bf29c78156
Philip Dakowitz <dakowitz@subshell.com>
2022-06-30 16:18:23 +0200

    add more metrics to the k8s parts

--- src/main/java/io/oneko/kubernetes/impl/DeploymentStatusWatcher.java
+++ src/main/java/io/oneko/kubernetes/impl/DeploymentStatusWatcher.java
@@ -1,10 +1,9 @@
 package io.oneko.kubernetes.impl;
 
-import static io.oneko.util.MoreStructuredArguments.projectKv;
-import static io.oneko.util.MoreStructuredArguments.versionKv;
-import static net.logstash.logback.argument.StructuredArguments.kv;
-import static net.logstash.logback.argument.StructuredArguments.v;
+import static io.oneko.util.MoreStructuredArguments.*;
+import static net.logstash.logback.argument.StructuredArguments.*;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,6 +11,8 @@ import java.util.stream.Collectors;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import io.oneko.event.CurrentEventTrigger;
 import io.oneko.event.EventTrigger;
 import io.oneko.event.ScheduledTask;
@@ -24,6 +25,7 @@ import io.oneko.kubernetes.deployments.DeploymentRepository;
 import io.oneko.kubernetes.deployments.DesiredState;
 import io.oneko.kubernetes.deployments.ReadableDeployment;
 import io.oneko.kubernetes.deployments.WritableDeployment;
+import io.oneko.metrics.MetricNameBuilder;
 import io.oneko.project.ProjectRepository;
 import io.oneko.project.ProjectVersion;
 import io.oneko.project.ProjectVersionLock;
@@ -31,12 +33,10 @@ import io.oneko.project.ReadableProject;
 import io.oneko.project.WritableProjectVersion;
 import io.oneko.websocket.SessionWebSocketHandler;
 import io.oneko.websocket.message.DeploymentStatusChangedMessage;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
-@AllArgsConstructor
 class DeploymentStatusWatcher {
 
 	private final ProjectRepository projectRepository;
@@ -47,21 +47,48 @@ class DeploymentStatusWatcher {
 	private final HelmCommands helmCommands;
 	private final ProjectVersionLock projectVersionLock;
 
+	private final Timer checkDeploymentStatusTimer;
+
+	DeploymentStatusWatcher(ProjectRepository projectRepository,
+													DeploymentRepository deploymentRepository,
+													SessionWebSocketHandler webSocketHandler,
+													HelmStatusToDeploymentMapper helmStatusToDeploymentMapper,
+													CurrentEventTrigger currentEventTrigger,
+													HelmCommands helmCommands,
+													ProjectVersionLock projectVersionLock,
+													MeterRegistry meterRegistry) {
+		this.projectRepository = projectRepository;
+		this.deploymentRepository = deploymentRepository;
+		this.webSocketHandler = webSocketHandler;
+		this.helmStatusToDeploymentMapper = helmStatusToDeploymentMapper;
+		this.currentEventTrigger = currentEventTrigger;
+		this.helmCommands = helmCommands;
+		this.projectVersionLock = projectVersionLock;
+		this.checkDeploymentStatusTimer = Timer.builder(new MetricNameBuilder().durationOf("kubernetes.deployment.status.check").build())
+				.description("the time it takes to iterate over all relevant deployments and update their status")
+				.publishPercentileHistogram()
+				.minimumExpectedValue(Duration.ofMillis(500))
+				.register(meterRegistry);
+	}
+
+
 	private EventTrigger asTrigger() {
 		return new ScheduledTask("Kubernetes Deployment Status Watcher");
 	}
 
 	@Scheduled(fixedRate = 10_000)
 	protected void updateProjectStatus() {
-		final List<WritableProjectVersion> writableVersions = projectRepository.getAll().stream()
-				.map(ReadableProject::writable)
-				.flatMap(writableProject -> writableProject.getVersions().stream())
-				.filter(this::shouldScanDeployable)
-				.collect(Collectors.toList());
+		checkDeploymentStatusTimer.record(() -> {
+			final List<WritableProjectVersion> writableVersions = projectRepository.getAll().stream()
+					.map(ReadableProject::writable)
+					.flatMap(writableProject -> writableProject.getVersions().stream())
+					.filter(this::shouldScanDeployable)
+					.collect(Collectors.toList());
 
-		try (var ignored = currentEventTrigger.forTryBlock(asTrigger())) {
-			writableVersions.forEach(this::scanResourcesForDeployable);
-		}
+			try (var ignored = currentEventTrigger.forTryBlock(asTrigger())) {
+				writableVersions.forEach(this::scanResourcesForDeployable);
+			}
+		});
 	}
 
 	private boolean shouldScanDeployable(ProjectVersion<?, ?> projectVersion) {
