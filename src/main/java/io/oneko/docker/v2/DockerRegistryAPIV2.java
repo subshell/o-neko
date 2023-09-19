9f151af5e735edd05d610a18b5b78cee3b06d496
Philip Dakowitz <dakowitz@subshell.com>
2023-09-19 11:53:02 +0200

    fixes polling of multiplatform images

--- src/main/java/io/oneko/docker/v2/DockerRegistryAPIV2.java
+++ src/main/java/io/oneko/docker/v2/DockerRegistryAPIV2.java
@@ -1,5 +1,6 @@
 package io.oneko.docker.v2;
 
+import feign.Headers;
 import feign.Param;
 import feign.RequestLine;
 import io.oneko.docker.v2.model.ListTagsResult;
@@ -8,16 +9,19 @@ import io.oneko.docker.v2.model.manifest.DockerRegistryManifest;
 
 public interface DockerRegistryAPIV2 {
 
-	@RequestLine("GET /v2/")
-	String versionCheck();
+    @RequestLine("GET /v2/")
+    String versionCheck();
 
-	@RequestLine("GET /v2/{imageName}/tags/list")
-	ListTagsResult getAllTags(@Param("imageName") String imageName);
+    @RequestLine("GET /v2/{imageName}/tags/list")
+    ListTagsResult getAllTags(@Param("imageName") String imageName);
 
-	@RequestLine("GET /v2/{imageName}/manifests/{tagName}")
-	DockerRegistryManifest getManifest(@Param("imageName") String imageName, @Param("tagName") String tagName);
+    @RequestLine("GET /v2/{imageName}/manifests/{tagName}")
+    @Headers({
+            "Accept: application/vnd.oci.image.manifest.v1+json, application/vnd.docker.distribution.manifest.v2+json, application/vnd.docker.distribution.manifest.list.v2+json"
+    })
+    DockerRegistryManifest getManifest(@Param("imageName") String imageName, @Param("tagName") String tagName);
 
-	@RequestLine("GET /v2/{imageName}/blobs/{algorithm}:{digest}")
-	DockerRegistryBlob getBlob(@Param("imageName") String imageName, @Param("algorithm") String algorithm, @Param("digest") String digest);
+    @RequestLine("GET /v2/{imageName}/blobs/{algorithm}:{digest}")
+    DockerRegistryBlob getBlob(@Param("imageName") String imageName, @Param("algorithm") String algorithm, @Param("digest") String digest);
 
 }
--- src/main/java/io/oneko/docker/v2/model/manifest/DockerRegistryManifest.java
+++ src/main/java/io/oneko/docker/v2/model/manifest/DockerRegistryManifest.java
@@ -1,39 +1,69 @@
 package io.oneko.docker.v2.model.manifest;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
 import org.apache.commons.lang3.StringUtils;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import lombok.Data;
+import java.util.List;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class DockerRegistryManifest {
+    @Data
+    static class Config {
+        String digest;
+        String mediaType;
+        int size;
+    }
 
-	@Data
-	static class Config {
-		String digest;
-	}
+    @Data
+    static class Platform {
+        String architecture;
+        String os;
+    }
 
-	@Data
-	public static class Digest {
-		String algorithm;
-		String digest;
+    @Data
+    public static class Manifest {
+        String digest;
+        String mediaType;
+        Platform platform;
+        int size;
 
-		public Digest(String digest) {
-			final String[] split = StringUtils.defaultString(digest).split(":");
-			this.algorithm = StringUtils.defaultString(split[0]);
-			this.digest = StringUtils.defaultString(split[1]);
-		}
+        public Digest getDigest() {
+            return new Digest(digest);
+        }
+    }
 
-		public String getFullDigest() {
-			return algorithm + ":" + digest;
-		}
-	}
+    @Data
+    public static class Digest {
+        String algorithm;
+        String digest;
 
-	private Config config;
+        public Digest(String digest) {
+            final String[] split = StringUtils.defaultString(digest).split(":");
+            this.algorithm = StringUtils.defaultString(split[0]);
+            this.digest = StringUtils.defaultString(split[1]);
+        }
 
-	public Digest getDigest() {
-		return new Digest(config.digest);
-	}
+        public String getFullDigest() {
+            return algorithm + ":" + digest;
+        }
+    }
+
+
+    private String mediaType;
+    private Config config;
+    private List<Manifest> manifests;
+
+
+    public Digest getDigest() {
+        if (!isManifestList()) {
+            return new Digest(config.digest);
+        }
+        throw new IllegalStateException("unsupported mediaType: " + mediaType);
+    }
+
+    public boolean isManifestList() {
+        return manifests != null && !manifests.isEmpty();
+    }
 }
